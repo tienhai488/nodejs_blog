f55ce3cfd3cbbde7a4e05568c931da7e07dbc9c8
Tien Hai <tienhai488@gmail.com>
2022-08-15 16:19:38 +0700

    setup ejs

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+// const handlebars = require("express-handlebars");
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "resources/view"));
+
+app.get("/", (req, res) => {
+  return res.render("home.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
